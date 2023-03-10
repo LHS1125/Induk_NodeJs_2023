f9d69cd85f5ef841ab89b81b1eced593ae2eba49
LHS1125 <81518061+LHS1125@users.noreply.github.com>
2023-03-10 21:23:28 +0900

    modify
    코드 수정해보기

--- ch1/callstack.js
+++ ch1/callstack.js
@@ -1,10 +1,10 @@
 function first() {
+  console.log( '첫 번째');
   second();
-  console.log('첫 번째');
 }
 function second() {
-  third();
   console.log('두 번째');
+  third();
 }
 function third() {
   console.log('세 번째');
