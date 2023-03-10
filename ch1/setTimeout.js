f0e32945259ff2041869dfe8f1381200ead15cbe
LHS1125 <81518061+LHS1125@users.noreply.github.com>
2023-03-10 21:55:51 +0900

    Update setTimeout.js

--- ch1/setTimeout.js
+++ ch1/setTimeout.js
@@ -1,6 +1,6 @@
 function run() {
-  console.log('3초 후 실행');
+  console.log('5초 후 실행');
 }
 console.log('시작');
-setTimeout(run, 3000);
+setTimeout(run, 5000); // 비동기, 설정한 interval이 지난후 해당되는 함수를 실행
 console.log('끝');
