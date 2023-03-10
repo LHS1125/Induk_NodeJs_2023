a904d6c5b3bf42188290d430bef59bf11d7b866f
LHS1125 <81518061+LHS1125@users.noreply.github.com>
2023-03-10 22:21:44 +0900

    blocking_nonblock

--- ch1/blocking.js
+++ ch1/blocking.js
@@ -1,3 +1,5 @@
+// 블로킹 I/O 표현(순서 주석표시)
+
 function longRunningTask() {
   // 오래 걸리는 작업
   console.log('작업 끝');
@@ -5,3 +7,9 @@ function longRunningTask() {
 console.log('시작');
 longRunningTask();
 console.log('다음 작업');
+
+
+//------- 결과 ----------
+// 시작
+// 작업 끝
+// 다음 작업
--- ch1/nonblocking.js
+++ ch1/nonblocking.js
@@ -1,7 +1,16 @@
-function longRunningTask() {
+// setTimeout을 이용한 논블로킹 I/O 표현(순서 주석표시)
+
+function longRunningTask(nStep){
   // 오래 걸리는 작업
-  console.log('작업 끝');
+  console.log(nStep, '번째 작업 끝');
 }
 console.log('시작');
-setTimeout(longRunningTask, 0);
-console.log('다음 작업');
+setTimeout(longRunningTask, 0, 1);
+console.log('다음 작업'); 
+setTimeout(longRunningTask, 0, 2);
+
+//-------- 결과 -------
+// 시작
+// 다음 작업
+// 1 번째 작업 끝
+// 2 번째 작업 끝
