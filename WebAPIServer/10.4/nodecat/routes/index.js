691e2dcaa3da69ef3998d574b5331ce9cdef6f77
LHS1125 <gh1369tjd@gmail.com>
2023-06-14 17:36:07 +0900

    update

--- WebAPIServer/10.4/nodecat/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { test } = require('../controllers');
-
-const router = express.Router();
-
-// POST /test
-router.get('/test', test);
-
-module.exports = router;
